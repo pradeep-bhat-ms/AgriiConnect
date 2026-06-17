1926685cb87a84d1bcbb5c1eb6123fb11e8d92c2
prdeep bhat m s <pradeephsgr@gmail.com>
2026-06-17 23:12:10 +0530

    Fix map component

--- frontend/src/screens/Map/Map.jsx
+++ frontend/src/screens/Map/Map.jsx
@@ -1,92 +1,115 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Image } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Image } from 'react-bootstrap';
 import { GoogleMap, Marker, InfoWindow, useJsApiLoader } from '@react-google-maps/api';
-import Message from './../../components/Message/Message'
-import Loader from './../../components/Loader/Loader'
-import { listSupplierProductsForAll } from './../../actions/supplierProduct'
-import MapStyles from './MapStyles'
-import Rating from './Rating/Rating'
-import { withScriptjs, withGoogleMap } from "react-google-maps";
+import Message from './../../components/Message/Message';
+import Loader from './../../components/Loader/Loader';
+import { listSupplierProductsForAll } from './../../actions/supplierProduct';
+import MapStyles from './MapStyles';
+import Rating from './Rating/Rating';
 
+const containerStyle = {
+  width: '100%',
+  height: '500px'
+};
 
-const Map = () => {
+const center = {
+  lat: 12.9716,
+  lng: 77.5946
+};
 
-    const dispatch = useDispatch()
+const MapComponent = () => {
+  const dispatch = useDispatch();
+  const [selectedPlace, setSelectedPlace] = useState(null);
 
-    const [selectedPlace, setSelectedPlace] = useState(null)
+  const supplierProductForAllList = useSelector(
+    state => state.supplierProductForAllList || {}
+  );
+  const {
+    loading: loadingProducts,
+    error: errorProducts,
+    products = []
+  } = supplierProductForAllList;
 
-    const supplierProductForAllList = useSelector(state => state.supplierProductForAllList)
-    const { loading: loadingProducts, error: errorProducts, products } = supplierProductForAllList
+  useEffect(() => {
+    dispatch(listSupplierProductsForAll());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(listSupplierProductsForAll())
-    }, [dispatch])
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY || ''
+  });
 
-    return (
-        <GoogleMap
-            defaultCenter={{ lat: 12.9716, lng: 77.5946}}
-            defaultZoom={10}
-            defaultOptions={{ styles: MapStyles }}
+  if (!isLoaded) {
+    return <div>Loading map...</div>;
+  }
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      options={{ styles: MapStyles }}
+    >
+      {loadingProducts ? (
+        <Loader />
+      ) : errorProducts ? (
+        <Message variant="danger">{errorProducts}</Message>
+      ) : (
+        Array.isArray(products) &&
+        products.map((place) => {
+          if (!place?.latitude || !place?.longitude) return null;
+
+          return (
+            <Marker
+              key={place._id}
+              position={{
+                lat: Number(place.latitude),
+                lng: Number(place.longitude)
+              }}
+              onClick={() => setSelectedPlace(place)}
+              icon={{
+                url: '/mapIcon.svg',
+                scaledSize: new window.google.maps.Size(25, 25)
+              }}
+            />
+          );
+        })
+      )}
+
+      {selectedPlace && (
+        <InfoWindow
+          position={{
+            lat: Number(selectedPlace.latitude),
+            lng: Number(selectedPlace.longitude)
+          }}
+          onCloseClick={() => setSelectedPlace(null)}
         >
-            {
-                loadingProducts ? <Loader />
-                    : errorProducts
-                        ? <Message variant='danger'>{errorProducts}</Message>
-                        : (
-                            products.map(place => (
-                                <Marker
-                                    key={place._id}
-                                    position={{
-                                        lat: place.latitude,
-                                        lng: place.longitude
-                                    }}
-                                    onClick={() => {
-                                        setSelectedPlace(place)
-                                    }}
-                                    icon={{
-                                        url: '/mapIcon.svg',
-                                        scaledSize: new window.google.maps.Size(25, 25)
-                                    }}
-                                />
-                            ))
-                        )
+          <div>
+            <Image
+              className="mx-auto d-block img-fluid mb-1"
+              rounded
+              width="120px"
+              src={selectedPlace.image}
+              alt={selectedPlace.name}
+            />
+            <h4 style={{ textAlign: 'center' }}>
+              {selectedPlace.cropSelection}
+            </h4>
+            <p>
+              Description: {selectedPlace.description}
+              <br />
+              {selectedPlace.isReviwed && (
+                <Rating
+                  text="Rating"
+                  value={selectedPlace.rating}
+                />
+              )}
+            </p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+};
 
-            }
-            {
-                selectedPlace && (
-                    <InfoWindow
-                        position={{
-                            lat: selectedPlace.latitude,
-                            lng: selectedPlace.longitude
-                        }}
-                        onCloseClick={() => {
-                            setSelectedPlace(null)
-                        }}
-                    >
-                        <div>
-                            <Image className="mx-auto d-block img-fluid mb-1" rounded width="120px" src={selectedPlace.image} alt={selectedPlace.name} />
-                            <h4 style={{ textAlign: "center" }}>{selectedPlace.cropSelection}</h4>
-                            <p>
-                                Description: {selectedPlace.description}<br />
-                                {
-                                    selectedPlace.isReviwed
-                                        ? (<>
-                                            <p><Rating text="Rating" value={selectedPlace.rating} /></p>
-                                        </>
-                                        )
-                                        : ''
-                                }
-                            </p>
-                        </div>
-                    </InfoWindow>
-                )
-            }
-        </GoogleMap>
-
-    )
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(Map))
-
-export default WrappedMap
+export default MapComponent;
